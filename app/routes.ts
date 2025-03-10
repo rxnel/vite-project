d8cd7785da812e3dfaec476139de1ac3da9ca632
ronel <ronel.aguilar@icloud.com>
2025-03-10 15:45:06 -0400

    init nested routes

--- app/routes.ts
+++ app/routes.ts
@@ -4,4 +4,10 @@ export default [
 	index('routes/home.tsx'),
 	route('about', 'routes/about.tsx'),
 	route('post/:postId', 'routes/post.tsx'),
+
+	// Nested Route Example with Dashboard
+	route('dashboard', 'routes/dashboard.tsx', [
+		route('user-settings', 'routes/user-settings.tsx'),
+		route('user-stats', 'routes/user-stats.tsx'),
+	]),
 ] satisfies RouteConfig;
