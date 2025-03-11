ef1510583ee999566d6a675f312070bf30dd498d
ronel <ronel.aguilar@icloud.com>
2025-03-11 11:43:11 -0400

    connecting to supabase and fetching data

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,3 +1,36 @@
-export default function About() {
-	return <p>About</p>;
+import { useState, useEffect } from 'react';
+import supabase from '../utils/supabase';
+
+interface Todo {
+	id: number;
+	created_at: string;
+	title: string;
+	completed: boolean;
 }
+
+function About() {
+	const [todos, setTodos] = useState<Todo[]>([]);
+
+	useEffect(() => {
+		async function getTodos() {
+			let { data: Todos, error } = await supabase
+				.from('Todos')
+				.select('*')
+				.order('created_at', { ascending: false });
+			if (Todos) {
+				setTodos(Todos as Todo[]);
+			}
+		}
+
+		getTodos();
+	}, []);
+
+	return (
+		<div>
+			{todos.map((todo) => (
+				<li key={todo.id}>{todo.title}</li>
+			))}
+		</div>
+	);
+}
+export default About;
